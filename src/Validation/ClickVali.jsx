63300573e049bd85ea2b386fa3d4e0b0a10ddc6b
Parag Tank <122973953+Parag-001@users.noreply.github.com>
2023-02-28 14:50:18 +0530

    Update ClickVali.jsx

--- src/Validation/ClickVali.jsx
+++ src/Validation/ClickVali.jsx
@@ -1,10 +1,4 @@
 const ClickValidation = (val, k) => {
-  // Object.values(val).every((c) => {
-  //   if (c === "") {
-  //     return;
-  //   }
-  // });
-  // console.log("val.name :>> ", val.name);
   return Object.values(val).map((c) => (c === "" ? "Please Enter Data" : ""));
 };
 
